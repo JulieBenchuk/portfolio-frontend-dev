65e2fcfb757bf219b228fd8f266cb5091b1c5451
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-30 22:52:57 +0300

    add button 'MORE ABOUT ME'

--- src/description/description.js
+++ src/description/description.js
@@ -3,13 +3,34 @@ import style from "./description.module.css"
 
 
 export const Description = () => {
-    return (
-        <div className={style.description}>
+    return (<div className={style.description}>
             <span className={style.photo}>
-                Photo
-                <img src="./../img/2022-03-11%2013.55.34.jpg"/>
+                <img className={style.avatar} src="https://c.neh.tw/thumb/f/720/comvecteezy227852.jpg"/>
             </span>
-            <span className={style.text}>Text</span>
-        </div>
-    );
+        <span className={style.text}>
+                <h1>
+                    <span>-</span> <span>I'M JULIE.</span>
+                   <p>FRONT-END DEVELOPER</p>
+                </h1>
+                <p>Strong proficiency in JavaScript, including DOM manipulation and the JavaScript object model
+Thorough understanding of React.js and its core principles
+Prior experience with popular React.js workflows (such as Flux or Redux)
+Familiarity with more current specifications of EcmaScript
+Prior experience with data structure libraries (e.g., Immutable.js)
+Knowledge of isomorphic React is a plus
+Familiarity with RESTful APIs
+Familiarity with HTML / CSS
+GIT experience is a plus
+Knowledge of modern authorization mechanisms, such as JSON Web Token
+Familiarity with modern front-end build pipelines and tools
+Experience with common front-end development tools such as Babel, Webpack, NPM, etc.
+Ability to understand business requirements and translate them into technical requirements
+A knack for benchmarking and optimization
+Familiarity with code versioning tools (such as Git, SVN, and Mercurial)
+                 </p>
+           <div className={style.button}>
+               <a href="">More about me <img className={style.button_icon} src="https://i.pinimg.com/originals/6a/7c/16/6a7c16de4754436ce05f59fbf3098b21.png"/></a>
+           </div>
+            </span>
+    </div>);
 }
