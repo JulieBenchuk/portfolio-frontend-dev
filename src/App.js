28c2ef5b5c378ded35d115c07dcbeec37f9fa327
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-30 21:38:08 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
+import React from "react";
+import {Header} from "./header/Header";
+import style from "./App.module.css"
+import {Description} from "./description/description";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={style.app}>
+            <Header/>
+            <Description/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/description/description.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./description.module.css"
+
+
+export const Description = () => {
+    return (
+        <div className={style.description}>
+            <div>Photo</div>
+            <div>Text</div>
+        </div>
+    );
+}
