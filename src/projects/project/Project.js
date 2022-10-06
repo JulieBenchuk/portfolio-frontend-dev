372ddfc857c370871d5cece18517fc70561c8214
JulieBenchuk <juliebenchuk@gmail.com>
2022-10-06 21:43:54 +0300

    refactor: css for buttons (projects)

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import style from "./Project.module.css"
+import homeStyle from "./../../App.module.css"
 
 
 export const Project = (props) => {
     return (
         <div className={style.projectItem}>
             <div style={props.style} className={style.image}>
-                <div className={style.button}>Button</div>
+                <div className={style.button}>
+                    <a href="" className={`${style.btn_ref} ${homeStyle.home_btn_border} ${homeStyle.home_color}`}>See more</a>
+                </div>
             </div>
             <div className={style.description}>
                 <h5 className={style.title}>{props.name}</h5>
