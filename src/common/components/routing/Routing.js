03597409466f3709714b5b00f1cd441ea536eeae
JulieBenchuk <juliebenchuk@gmail.com>
2022-10-21 20:52:35 +0300

    feat: added slide animation

--- src/common/components/routing/Routing.js
+++ src/common/components/routing/Routing.js
@@ -4,17 +4,20 @@ import {Description} from "../../../description/Description";
 import {Skills} from "../../../skills/Skills";
 import {Projects} from "../../../projects/Projects";
 import {Contacts} from "../../../contacts/Contacts";
+import Slide from 'react-reveal/Slide';
 
 export const Routing = () => {
     return (
         <div>
-            <Routes>
+            <Slide bottom>
+                <Routes>
                 <Route path="/description" element={<Description/>}/>
                 <Route path="/skills" element={<Skills/>}/>
                 <Route path="/projects" element={<Projects/>}/>
                 <Route path="/contacts" element={<Contacts/>}/>
                 <Route path="*" element={<Navigate to="/description"/>}/>
             </Routes>
+            </Slide>
         </div>
     );
 };
