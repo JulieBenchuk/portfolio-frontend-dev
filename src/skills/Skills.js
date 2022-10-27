4e464a603b1b959387df4c27d90e639801f4fb07
JulieBenchuk <juliebenchuk@gmail.com>
2022-10-27 12:37:12 +0300

    refactor: added new skills

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styleContainer from "../common/styles/container/container.module.scss"
-import style from "./Skills.module.scss"
+import style from "./Skills.module.scss";
 import {Skill} from "./skill/Skill";
 import {Title} from "../common/components/title/Title";
 import {faReact} from "@fortawesome/free-brands-svg-icons";
@@ -9,10 +9,13 @@ import {faJs} from "@fortawesome/free-brands-svg-icons";
 import {faCss3Alt} from "@fortawesome/free-brands-svg-icons";
 import {faSass} from "@fortawesome/free-brands-svg-icons";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faAngular} from "@fortawesome/free-brands-svg-icons";
 import {faGlobe} from "@fortawesome/free-solid-svg-icons";
-import {redux_icon} from "../assets/img/svg/redux"
-import {ts_icon} from "../assets/img/svg/ts"
+import {redux_icon} from "../assets/img/svg/redux";
+import {ts_icon} from "../assets/img/svg/ts";
 import {test_icon} from "../assets/img/svg/test"
+import {mui_icon} from "../assets/img/svg/mui"
+import {heroku_icon} from "../assets/img/svg/heroku"
 import Slide from 'react-reveal/Slide';
 import {Info} from "./info/Info";
 import {Achievement} from "./achievement/Achievement";
@@ -79,19 +82,25 @@ export const Skills = () => {
                             <Skill title={"Redux"} icon_svg={redux_icon}
                                    description={"Experience in Redux and Redux Thunk."}/>
                             <Skill title={"TypeScript"} icon_svg={ts_icon}
-                                   description={"Basic Typescript knowledge."}/>
+                                   description={"Strong Typescript knowledge."}/>
+                            <Skill title={"REST API"} icon={faGlobe}
+                                   description={"Experience in working with RESTful services. Axios."}/>
+                            <Skill title={"Angular"} icon={faAngular}
+                                   description={"Basic Angular knowledge."}/>
                             <Skill title={"HTML"} icon={faHtml5}
                                    description={"HTML5, DOM, Semantic HTML."}/>
                             <Skill title={"CSS"} icon={faCss3Alt}
-                                   description={"Responsive design. CSS-modules."}/>
+                                   description={"CSS3. Responsive design. CSS-modules."}/>
                             <Skill title={"Sass/SCSS"} icon={faSass}
                                    description={"Experience in CSS preprocessor like SASS, SCSS."}/>
+                            <Skill title={"UI"} icon_svg={mui_icon}
+                                   description={"Knowledge of UI libraries (Material-UI, Storybook)."}/>
                             <Skill title={"Git"} icon={faGithub}
                                    description={"Familiarity with code versioning tool such as Git."}/>
+                            <Skill title={"Heroku"} icon_svg={heroku_icon}
+                                   description={"Experience in servless (Heroku)."}/>
                             <Skill title={"Testing"} icon_svg={test_icon}
                                    description={"Experience with unit testing, API testing (Postman)."}/>
-                            <Skill title={"REST API"} icon={faGlobe}
-                                   description={"Experience in working with RESTful services."}/>
                         </div>
                     </div>
                 </div>
